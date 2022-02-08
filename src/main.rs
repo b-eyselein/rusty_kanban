6f30c768ead3455726000eda7b4a585e995a773e
Björn Eyselein <bjoern.eyselein@uni-wuerzburg.de>
2022-02-08 12:10:33 +0100

    update routes for production

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 #[macro_use]
 extern crate diesel;
 
+use std::path::Path;
+#[cfg(not(debug_assertions))]
+use std::path::PathBuf;
+
 use diesel::PgConnection;
 use juniper::EmptySubscription;
-use juniper_rocket::{graphiql_source, GraphQLRequest, GraphQLResponse};
-use rocket::{get, launch, post, routes, State};
+#[cfg(debug_assertions)]
+use juniper_rocket::graphiql_source;
+use juniper_rocket::{GraphQLRequest, GraphQLResponse};
+use lazy_static::lazy_static;
+#[cfg(not(debug_assertions))]
+use rocket::fs::NamedFile;
+#[cfg(not(debug_assertions))]
+use rocket::response::status::NotFound;
+use rocket::{get, launch, post, routes, Route, State};
+#[cfg(debug_assertions)]
+use rocket::{response::Redirect, uri};
 use rocket_cors::CorsOptions;
 use rocket_sync_db_pools::database;
 
@@ -14,9 +27,42 @@ mod graphql;
 mod model;
 mod schema;
 
+lazy_static! {
+    static ref STATIC_BASE_PATH: &'static Path = Path::new("static");
+}
+
 #[database("kanban")]
 pub struct DbConn(PgConnection);
 
+#[cfg(debug_assertions)]
+#[get("/")]
+fn route_index() -> Redirect {
+    Redirect::to(uri!(route_graphiql))
+}
+
+#[cfg(not(debug_assertions))]
+#[get("/")]
+async fn route_index() -> Result<NamedFile, NotFound<String>> {
+    route_get_file("index.html".into()).await
+}
+
+#[cfg(not(debug_assertions))]
+#[get("/<file..>", rank = 2)]
+async fn route_get_file(file: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let file_path = STATIC_BASE_PATH.join(&file);
+
+    let named_file_result = if file_path.exists() {
+        NamedFile::open(file_path)
+    } else {
+        NamedFile::open(STATIC_BASE_PATH.join("index.html"))
+    };
+
+    named_file_result
+        .await
+        .map_err(|_error| NotFound(format!("file {} could not be found!", &file.display())))
+}
+
+#[cfg(debug_assertions)]
 #[get("/graphiql")]
 fn route_graphiql() -> rocket::response::content::Html<String> {
     graphiql_source("/graphql", None)
@@ -27,6 +73,16 @@ async fn route_post_graphql(connection: DbConn, request: GraphQLRequest, schema:
     request.execute(schema, &MyGraphQLContext::new(connection)).await
 }
 
+#[cfg(debug_assertions)]
+fn gather_routes() -> Vec<Route> {
+    routes![route_index, route_graphiql, route_post_graphql]
+}
+
+#[cfg(not(debug_assertions))]
+fn gather_routes() -> Vec<Route> {
+    routes![route_index, route_post_graphql, route_get_file]
+}
+
 #[launch]
 fn rocket() -> _ {
     let cors = CorsOptions { ..CorsOptions::default() }.to_cors().expect("Could not build CORS!");
@@ -35,5 +91,5 @@ fn rocket() -> _ {
         .attach(DbConn::fairing())
         .attach(cors)
         .manage(Schema::new(Query, Mutation, EmptySubscription::new()))
-        .mount("/", routes![route_graphiql, route_post_graphql])
+        .mount("/", gather_routes())
 }
